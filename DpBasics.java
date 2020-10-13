722e2f43c6b173cf22cb02ad550412128c02cc13
sriapul <sridharjayapaul@gmail.com>
2020-10-12 17:07:57 -0700

    Update DpBasics.java

--- DpBasics.java
+++ DpBasics.java
@@ -3,4 +3,20 @@
 /**
 Fibonacci series for basic dp explanation
 
+DP basics:
+
+1. Remember previous answers
+2. To solve big one think what it would be the reaction  if the problem is small and predict a answer
+3. Complexity of brute force will be TC: O(# of recurive call ^ depth) - overlapping subproblems  and for tabulation O( row ^ col) => 
+4. Optimize => trading of space to reduce time
+5. How to reduce exponential to linear time
+
+Types:
+1. All possible answers - Coin change or decode ways
+2. min or max out of possiblity - Coin change
+3. Whether you will able to acheiv one valid answer
+
 **/
+
+
+
